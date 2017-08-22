c1843620ed166c9b48217002c36ff6e6d15cece0
gopraew <gopraew@gmail.com>
2017-08-22 15:47:21 +0700

    login แล้วเด้ง หน้าแรก success

--- app/src/main/java/siam/go/mint/num/siamdailynew/ServiceActivity.java
+++ app/src/main/java/siam/go/mint/num/siamdailynew/ServiceActivity.java
@@ -3,11 +3,20 @@ package siam.go.mint.num.siamdailynew;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import siam.go.mint.num.siamdailynew.fragment.NewsFragment;
+
 public class ServiceActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
+
+        //add fragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().add(R.id.serviceContent, new NewsFragment()).commit();
+        }
+
+
     }
-}
+} //mian class
